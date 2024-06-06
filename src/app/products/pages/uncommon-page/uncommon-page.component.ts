e7a8784991093824f0bb3bcba50d22652d8f390d
Javier Aranda <javierarandalopez96@gmail.com>
2024-06-06 18:47:41 +0200

    añadidos i18n select y plural Pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18n Select
+  public name:string ="Javi";
+  public gender: 'male' | 'female' ="male";
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+  changeClient():void{
+    this.name = 'Melissa';
+    this.gender= 'female';
+  }
+
+  clientLeft(){
+    this.clients.pop();
+  }
+
+//i18n Plural
+
+public clients : string[] =['Maria', 'Javi', 'Pedro', 'Nina','Laura', 'Vera', 'Edurne', 'Oscar'];
+public clientsMap = {
+  '=0': 'no tenemos ningun cliente esperando.',
+  '=1':'tenemos 1 cliente esperando.',
+  '=2': 'tenemos # personas esperando',
+  'other' : 'tenemos # clientes esperando.'
+}
+
 }
