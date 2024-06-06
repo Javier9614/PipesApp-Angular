ee4cf2e1750d71df9d7a5f20a48de8f901e0fbbb
Javier Aranda <javierarandalopez96@gmail.com>
2024-06-06 15:21:38 +0200

    Proyecto PipesApp

---
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-number-page',
+  templateUrl: './numbers-page.component.html',
+  styleUrl: './numbers-page.component.css'
+})
+export class NumbersPageComponent {
+
+}
