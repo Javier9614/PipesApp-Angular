ff455f71753ff1fb9f228392661bff34997df8c5
Javier Aranda <javierarandalopez96@gmail.com>
2024-06-06 19:30:06 +0200

    añadido Async Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'products-uncommon-page',
@@ -40,4 +41,18 @@ public person={
   address: 'Zaragoza, España'
 }
 
+// Async Pipe
+public myObservableTimer:Observable<number> = interval(2000).pipe(
+  tap(value => console.log('tap',value))
+);
+
+public promiseValue:Promise<string> = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+    resolve('Tenemos data en la promesa');
+    console.log('Tenemos data en la promesa');
+
+  },3500)
+})
+
 }
+
